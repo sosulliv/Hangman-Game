4331973428dd43010138140287fdcdf0aab9c8c4
Sean O'Sullivan <sosulliv@Seans-MacBook-Pro.local>
2017-11-05 23:18:38 -0500

    added hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -59,6 +59,8 @@ var loss_counter = 0;
 var win_counter = 0;
 var incorrect_guesses =10;
 var previous_word;
+var reset_flag;
+
 
 
 
@@ -70,6 +72,7 @@ var previous_word;
 function next_word()
 {  
     blanks =[];
+    reset_hangman();
 	//reset_table();
 	var random_index=Math.floor(Math.random() * states.length);
      
@@ -129,17 +132,21 @@ if (current_word.indexOf(letter) > -1)
 			console.log(wrong_guess_counter);
 		    document.getElementById('wrong_guess_count').innerHTML = wrong_guess_counter;
             document.getElementById('guess_remaining').innerHTML = (incorrect_guesses-wrong_guess_counter);
-
+            draw_hangman();
 			if  (wrong_guess_counter===incorrect_guesses ) {
                  loss_counter++;
                  document.getElementById('losses').innerHTML = loss_counter;
-
+                  draw_hangman();
+                  wrong_guess_counter=0;
+                 
                 console.log("GameOver");
 
 			}
 		}
 
 }		
+
+
 
  document.onkeyup = function(event) {
 
@@ -156,3 +163,133 @@ if (current_word.indexOf(letter) > -1)
 
 
 
+
+    	    
+function draw_hangman () {
+				var canvas, context, canvaso, contexto;
+				canvaso = document.getElementById('hangman_canvas');
+				context = canvaso.getContext('2d');
+				
+				if (wrong_guess_counter<11  && wrong_guess_counter >0) {
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(5, 361);
+				context.lineTo(189, 360);
+				context.stroke();
+				context.closePath();
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >1) {
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(92, 75);
+				context.lineTo(90, 355);
+				context.stroke();
+				context.closePath();
+
+				}
+
+
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >2) {
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(93, 73);
+				context.lineTo(268, 71);
+				context.stroke();
+				context.closePath();
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >3) {
+
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(267, 71);
+				context.lineTo(266, 105);
+				context.stroke();
+				context.closePath();
+
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >4) {
+				context.strokeStyle = '#000000';
+				context.save();
+				context.translate(260, 119);
+				context.scale(1, 0.6470588235294118);
+				context.beginPath();
+				context.arc(0, 0, 40, 0, 6.283185307179586, false);
+				context.stroke();
+				context.closePath();
+				context.restore();
+
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >5) {
+
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(264, 144);
+				context.lineTo(265, 266);
+				context.stroke();
+				context.closePath();
+
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >6) {
+
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(209, 317);
+				context.lineTo(265, 264);
+				context.stroke();
+				context.closePath();
+
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >7) {
+
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(267, 265);
+				context.lineTo(307, 325);
+				context.stroke();
+				context.closePath();
+				}
+
+				if (wrong_guess_counter<11  && wrong_guess_counter >8) {
+
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(267, 198);
+				context.lineTo(325, 162);
+				context.stroke();
+				context.closePath();
+
+				}
+
+				if (wrong_guess_counter<11   && wrong_guess_counter >9 ) {
+				context.strokeStyle = '#000000';
+				context.beginPath();
+				context.moveTo(208, 168);
+				context.lineTo(266, 200);
+				context.stroke();
+				context.closePath();
+				}
+
+				
+				
+           }
+
+
+function reset_hangman()
+
+
+
+{
+var c = document.getElementById("hangman_canvas");
+var ctx = c.getContext("2d");
+
+ctx.clearRect(0, 0, 600, 500);
+
+
+}
